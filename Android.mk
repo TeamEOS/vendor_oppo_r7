6426333039b60a5c8414ca0e4ec4e19a81ec2a1c
Christopher N. Hesse <raymanfx@gmail.com>
2015-07-05 00:07:57 +0200

    r5: Fix libtime_genoff path

--- Android.mk
+++ Android.mk
@@ -21,7 +21,7 @@ ifneq ($(filter r5,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := oppo
-LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_SRC_FILES := system/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
